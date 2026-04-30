95669eed4cd30c6dc2b9b01bbfaf1bd81aa264d6
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-04-30 12:32:24 +0530

    some modifications

--- modules/offboarder/outputs.tf
+++ modules/offboarder/outputs.tf
@@ -1,4 +1,4 @@
-output "execution_report" {
-  description = "The JSON response from the Lambda"
-  value = jsondecode(data.aws_lambda_invocation.offboard_trigger.result)
-}
+# output "execution_report" {
+#   description = "The JSON response from the Lambda"
+#   value = jsondecode(data.aws_lambda_invocation.offboard_trigger.result)
+# }
